7f5c13f50faf03a1dae77fba1a8ba5e8186f5074
angelpm14 <116526502+angelpm14@users.noreply.github.com>
2022-12-17 16:09:07 -0500

    Add files via upload

---
+++ cinestar_sweb.js
@@ -0,0 +1,86 @@
+// 20221217155725
+// https://gloglogloglo1.000webhostapp.com/cinestar_sweb/cines
+
+[
+  {
+    "id": "1",
+    "RazonSocial": "Cinestar Excelsior",
+    "Salas": "8",
+    "idDistrito": "4",
+    "Direccion": "Jr. De la Unión 780",
+    "Telefonos": "714-1865",
+    "Detalle": "Lima"
+  },
+  {
+    "id": "2",
+    "RazonSocial": "Cinestar Sur",
+    "Salas": "8",
+    "idDistrito": "8",
+    "Direccion": "Av Los Héroes 240",
+    "Telefonos": "710-9439",
+    "Detalle": "San Juan de Miraflores"
+  },
+  {
+    "id": "3",
+    "RazonSocial": "Cinestar Metro San Juan",
+    "Salas": "7",
+    "idDistrito": "7",
+    "Direccion": "Av. Los Proceres de la Independencia 1632",
+    "Telefonos": "719-1064",
+    "Detalle": "San Juan de Lurigancho"
+  },
+  {
+    "id": "4",
+    "RazonSocial": "Cinestar Metro UNI",
+    "Salas": "10",
+    "idDistrito": "5",
+    "Direccion": "Av. Gerardo Unger s/n cuadra 16",
+    "Telefonos": "719-5142",
+    "Detalle": "Rímac"
+  },
+  {
+    "id": "5",
+    "RazonSocial": "Cinestar Metro Comas",
+    "Salas": "6",
+    "idDistrito": "2",
+    "Direccion": "Av. Tupac Amaru cdra 39 sn - La Pascana",
+    "Telefonos": " 715-2602",
+    "Detalle": "Comas"
+  },
+  {
+    "id": "6",
+    "RazonSocial": "Cinestar Las Américas",
+    "Salas": "7",
+    "idDistrito": "3",
+    "Direccion": "Av. Hermilio Valdizán 121 (altura de la cuadra 18 de la Av. Brasil)",
+    "Telefonos": "714-1718",
+    "Detalle": "Jesús Maria"
+  },
+  {
+    "id": "7",
+    "RazonSocial": "Cinestar Aviación",
+    "Salas": "5",
+    "idDistrito": "6",
+    "Direccion": "Calle Tiziano 100 Alt. Cuadra 24 de la Av. Aviación",
+    "Telefonos": "715-8598",
+    "Detalle": "San Borja"
+  },
+  {
+    "id": "8",
+    "RazonSocial": "Cinestar Benavides",
+    "Salas": "6",
+    "idDistrito": "9",
+    "Direccion": "Av. Benavides 4981",
+    "Telefonos": "7141288",
+    "Detalle": "Santiago de Surco"
+  },
+  {
+    "id": "9",
+    "RazonSocial": "Cinestar Metro Breña",
+    "Salas": "8",
+    "idDistrito": "1",
+    "Direccion": "Jr. Iquique Nº 315 - Breña Av. Alfonso Ugarte y Venezuela",
+    "Telefonos": "719-2445",
+    "Detalle": "Breña"
+  }
+]
