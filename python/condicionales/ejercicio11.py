492c39c6f45c3956090a589eeb55dda6704c95ce
angelpm14 <116526502+angelpm14@users.noreply.github.com>
2022-12-17 14:37:47 -0500

    Update ejercicio11.py

---
+++ python/condicionales/ejercicio11.py
@@ -0,0 +1,14 @@
+#11.	Desarrolle el programa que determine el signo de un número entre positivo, negativo y cero.
+
+import os
+os.system("cls")
+'''11.	Desarrolle el programa que determine el signo de un número entre positivo, negativo y cero.'''
+print("")
+n= int(input("ingresa un numero: "))
+
+if n > 0:
+    print("el numero es positivo")
+elif n < 0:
+    print("el numero es negativo")
+else: 
+    print("el numero es neutro")
